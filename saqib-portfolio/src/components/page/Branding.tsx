dcc9312e0908ec50e75b508c030645506ef035e5
Saqib Mustafa Abbasi <saqibmabbasi@gmail.com>
2025-09-22 09:36:00 +0500

    Update Branding.tsx

--- saqib-portfolio/src/components/page/Branding.tsx
+++ saqib-portfolio/src/components/page/Branding.tsx
@@ -3,6 +3,6 @@ import branding from '../../data-files/branding.json';
 
 export default function Branding() {
     return <>
-        <A href={branding.name_route}><h3>{branding.name}</h3></A>
+        {<A href={branding.name_route}><h3>{branding.name}</h3></A>}
         </>
 }
