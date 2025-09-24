d572600f37fe88638cbb03ceec70d8b249d6bff3
Saqib Mustafa Abbasi <saqibmabbasi@gmail.com>
2025-09-24 13:43:50 +0500

    Tailwind and TSConfig changed

--- saqib-portfolio/tailwind.config.cjs
+++ saqib-portfolio/tailwind.config.cjs
@@ -1,11 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: [
+//         "./index.html",
+//         "./src/**/*.{js,ts,jsx,tsx}"  // SolidJS/Vite will look here
+//     ],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: [],
+// }
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}"  // SolidJS/Vite will look here
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // enables dark mode with "dark" class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
