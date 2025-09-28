b0b883d28a92168cc4efa3f989f0b5b29738ca59
Saqib Mustafa Abbasi <saqibmabbasi@gmail.com>
2025-09-28 15:13:42 +0500

    Styling - 1

--- saqib-portfolio/src/index.tsx
+++ saqib-portfolio/src/index.tsx
@@ -14,7 +14,7 @@ import Home from './pages/Home';
 import Project from './components/page/Project.tsx';
 import Projects from './pages/Projects';
 import Skills from './pages/Skills';
-import SocialLinks from './pages/SocialLinks';
+// import SocialLinks from './pages/SocialLinks';
 import NotFound from "./pages/NotFound";
 
 // import Header from "./components/layout/Header.tsx";
